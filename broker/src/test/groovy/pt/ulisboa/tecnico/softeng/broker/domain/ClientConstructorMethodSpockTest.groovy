fd2e87772f8069924f638e26daa78a2a1ae8e2df
PauloACDias <paulo.a.c.dias@tecnico.ulisboa.pt>
2019-03-13 15:08:31 +0000

    Fixes ClientConstructorMethodSpockTest #28

--- broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/ClientConstructorMethodSpockTest.groovy
+++ broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/ClientConstructorMethodSpockTest.groovy
@@ -1,28 +1,28 @@
-package pt.ulisboa.tecnico.softeng.broker.domain;
+package pt.ulisboa.tecnico.softeng.broker.domain
 
-import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException;
+import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException
 
 import spock.lang.Unroll
 import spock.lang.Shared
 
 class ClientConstructorMethodSpockTest extends SpockRollbackTestAbstractClass {
 
-  def BROKER_IBAN = "BROKER_IBAN";
-  def NIF_AS_BUYER = "buyerNIF";
-  def BROKER_NIF_AS_SELLER = "sellerNIF";
-  def IBAN_BUYER = "IBAN";
-  def OTHER_NIF = "987654321";
-  @Shared def CLIENT_NIF = "123456789";
-  @Shared def DRIVING_LICENSE = "IMT1234";
-  @Shared def AGE = 20;
-  @Shared def CLIENT_IBAN = "BK011234567";
+  def BROKER_IBAN = "BROKER_IBAN"
+  def NIF_AS_BUYER = "buyerNIF"
+  def BROKER_NIF_AS_SELLER = "sellerNIF"
+  def IBAN_BUYER = "IBAN"
+  def OTHER_NIF = "987654321"
+  @Shared def CLIENT_NIF = "123456789"
+  @Shared def DRIVING_LICENSE = "IMT1234"
+  @Shared def AGE = 20
+  @Shared def CLIENT_IBAN = "BK011234567"
 
-  protected Broker broker;
-  protected Client client;
+  protected Broker broker
+  protected Client client
 
   @Override
   def populate4Test() {
-    this.broker = new Broker("BR01", "eXtremeADVENTURE", BROKER_NIF_AS_SELLER, NIF_AS_BUYER, BROKER_IBAN);
+    this.broker = new Broker("BR01", "eXtremeADVENTURE", BROKER_NIF_AS_SELLER, NIF_AS_BUYER, BROKER_IBAN)
   }
 
   def 'success'() {
@@ -44,14 +44,13 @@ class ClientConstructorMethodSpockTest extends SpockRollbackTestAbstractClass {
       thrown(BrokerException)
 
     where:
-    broker      | iban        | nif        | drivingLicense  | age
-    null        | CLIENT_IBAN | CLIENT_NIF | DRIVING_LICENSE | AGE
-    this.broker | null        | CLIENT_NIF | DRIVING_LICENSE | AGE
-    this.broker | ""          | CLIENT_NIF | DRIVING_LICENSE | AGE
-    this.broker | CLIENT_IBAN | null       | DRIVING_LICENSE | AGE
-    this.broker | CLIENT_IBAN | ""         | DRIVING_LICENSE | AGE
-    this.broker | CLIENT_IBAN | CLIENT_NIF | DRIVING_LICENSE | -1
-    this.broker | CLIENT_IBAN | CLIENT_NIF | ""              | AGE
+	    broker      | iban        | nif        | drivingLicense  | age
+	    null        | CLIENT_IBAN | CLIENT_NIF | DRIVING_LICENSE | AGE    this.broker | null        | CLIENT_NIF | DRIVING_LICENSE | AGE
+	    this.broker | ""          | CLIENT_NIF | DRIVING_LICENSE | AGE
+	    this.broker | CLIENT_IBAN | null       | DRIVING_LICENSE | AGE
+	    this.broker | CLIENT_IBAN | ""         | DRIVING_LICENSE | AGE
+	    this.broker | CLIENT_IBAN | CLIENT_NIF | DRIVING_LICENSE | -1
+	    this.broker | CLIENT_IBAN | CLIENT_NIF | ""              | AGE
   }
 
   def 'client Exists With Same IBAN'() {
