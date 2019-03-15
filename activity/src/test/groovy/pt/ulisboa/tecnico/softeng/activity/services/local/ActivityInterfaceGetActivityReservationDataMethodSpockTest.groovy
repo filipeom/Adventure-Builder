3d1998938c95e9771d0679e50b7468534b639317
HusseinGiva <hussein.giva@tecnico.ulisboa.pt>
2019-03-15 14:33:56 +0000

    Fixes ActivityInterfaceGetActivityReservationDataMethodSpockTest, issue #11

--- activity/src/test/groovy/pt/ulisboa/tecnico/softeng/activity/services/local/ActivityInterfaceGetActivityReservationDataMethodSpockTest.groovy
+++ activity/src/test/groovy/pt/ulisboa/tecnico/softeng/activity/services/local/ActivityInterfaceGetActivityReservationDataMethodSpockTest.groovy
@@ -22,51 +22,57 @@ class ActivityInterfaceGetActivityReservationDataMethodSpockTest extends SpockRo
 
 	@Override
 	def populate4Test() {
-		this.provider = new ActivityProvider(CODE, NAME, "NIF", "IBAN")
-		def activity = new Activity(this.provider, "Bush Walking", 18, 80, 3)
-		this.offer = new ActivityOffer(activity, this.begin, this.end, 30)
+		provider = new ActivityProvider(CODE, NAME, "NIF", "IBAN")
+		def activity = new Activity(provider, "Bush Walking", 18, 80, 3)
+		offer = new ActivityOffer(activity, begin, end, 30)
 	}
 
 	def "success"() {
 		given:
-		this.booking = new Booking(this.provider, this.offer, "123456789", "IBAN")
+		booking = new Booking(provider, offer, "123456789", "IBAN")
 
 		when:
-		def data = ActivityInterface.getActivityReservationData(this.booking.getReference())
+		def data = ActivityInterface.getActivityReservationData(booking.getReference())
 
 		then:
-		data.getReference() == this.booking.getReference()
-		data.getCancellation() == null
-		data.getName() == NAME
-		data.getCode() == CODE
-		data.getBegin() == this.begin
-		data.getEnd() == this.end
-		data.getCancellationDate() == null
+		with (data) {
+			getReference() == booking.getReference()
+			getCancellation() == null
+			getName() == NAME
+			getCode() == CODE
+			getBegin() == begin
+			getEnd() == end
+			getCancellationDate() == null
+		}
 	}
 
 	def "successCancelled"() {
+		given:
+		booking = new Booking(provider, offer, "123456789", "IBAN")
+
 		when:
-		this.booking = new Booking(this.provider, this.offer, "123456789", "IBAN")
-		this.provider.getProcessor().submitBooking(this.booking)
-		this.booking.cancel()
-		def data = ActivityInterface.getActivityReservationData(this.booking.getCancel())
+		provider.getProcessor().submitBooking(booking)
+		booking.cancel()
+		def data = ActivityInterface.getActivityReservationData(booking.getCancel())
 
 		then:
-		data.getReference() == this.booking.getReference()
-		data.getCancellation() == this.booking.getCancel()
-		data.getName() == NAME
-		data.getCode() == CODE
-		data.getBegin() == this.begin
-		data.getEnd() == this.end
-		data.getCancellationDate() != null
+		with (data) {
+			getReference() == booking.getReference()
+			getCancellation() == booking.getCancel()
+			getName() == NAME
+			getCode() == CODE
+			getBegin() == begin
+			getEnd() == end
+			getCancellationDate() != null
+		}
 	}
 
 	@Unroll("Get Activity Reservation Data: #ref")
 	def "exceptions"(){
-		when: "retrieving Activity reservation data with invalid arguments"
+		when:
 		ActivityInterface.getActivityReservationData(ref)
 
-		then: "throws an exception"
+		then:
 		thrown(ActivityException)
 
 		where:
