721eb903ba9b165c1e058694a9b08815be9f2a31
PauloACDias <paulo.a.c.dias@tecnico.ulisboa.pt>
2019-03-15 11:56:56 +0000

    Fixes BookingConflictMethodSpockTest #29

--- hotel/src/test/groovy/pt/ulisboa/tecnico/softeng/hotel/domain/BookingConflictMethodSpockTest.groovy
+++ hotel/src/test/groovy/pt/ulisboa/tecnico/softeng/hotel/domain/BookingConflictMethodSpockTest.groovy
@@ -1,8 +1,8 @@
-package pt.ulisboa.tecnico.softeng.hotel.domain;
+package pt.ulisboa.tecnico.softeng.hotel.domain
 
-import org.joda.time.LocalDate;
+import org.joda.time.LocalDate
 
-import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException
 
 class BookingConflictMethodSpockTest extends SpockRollbackTestAbstractClass {
   def arrival = LocalDate.parse('2016-12-19')
@@ -21,82 +21,82 @@ class BookingConflictMethodSpockTest extends SpockRollbackTestAbstractClass {
 	}
 
   def 'arguments Are Consistent'() {
-    given:
-    def date1 = LocalDate.parse('2016-12-9')
-    def date2 = LocalDate.parse('2016-12-15')
+    when:
+      def date1 = LocalDate.parse('2016-12-9')
+      def date2 = LocalDate.parse('2016-12-15')
 
-    expect:
-    this.booking.conflict(date1, date2) == false
+    then:
+      this.booking.conflict(date1, date2) == false
   }
 
   def 'no Conflict Because It Is Cancelled'() {
-    given:
-    this.booking.cancel()
+    when:
+      this.booking.cancel()
 
-    expect:
-    this.booking.conflict(this.booking.getArrival(), this.booking.getDeparture()) == false
+    then:
+      this.booking.conflict(this.booking.getArrival(), this.booking.getDeparture()) == false
   }
 
   def 'arguments Are Inconsistent'() {
     when:
-    this.booking.conflict(new LocalDate(2016, 12, 15), new LocalDate(2016, 12, 9)) == true
+      this.booking.conflict(new LocalDate(2016, 12, 15), new LocalDate(2016, 12, 9)) == true
 
     then:
-    thrown(HotelException)
+      thrown(HotelException)
   }
 
   def 'arguments Same Day'() {
-    given:
-    def date1 = LocalDate.parse('2016-12-9')
-    def date2 = LocalDate.parse('2016-12-9')
+    when:
+      def date1 = LocalDate.parse('2016-12-9')
+      def date2 = LocalDate.parse('2016-12-9')
 
-    expect:
-    this.booking.conflict(date1, date2) == true
+    then:
+      this.booking.conflict(date1, date2) == true
   }
 
   def 'arrival And Departure Are Before Booked'() {
     expect:
-    this.booking.conflict(this.arrival.minusDays(10), this.arrival.minusDays(4)) == false
+      this.booking.conflict(this.arrival.minusDays(10), this.arrival.minusDays(4)) == false
   }
 
   def 'arrival And Departure Are Before Booked But Departure Is Equal To Booked Arrival'() {
     expect:
-    this.booking.conflict(this.arrival.minusDays(10), this.arrival) == false
+      this.booking.conflict(this.arrival.minusDays(10), this.arrival) == false
   }
 
   def 'arrival And Departure Are After Booked'() {
     expect:
-    this.booking.conflict(this.departure.plusDays(4), this.departure.plusDays(10)) == false
+      this.booking.conflict(this.departure.plusDays(4), this.departure.plusDays(10)) == false
   }
 
   def 'arrival And Departure Are After Booked But Arrival Is Equal To Booked Departure'() {
     expect:
-    this.booking.conflict(this.departure, this.departure.plusDays(10)) == false
+      this.booking.conflict(this.departure, this.departure.plusDays(10)) == false
   }
 
   def 'arrival Is Before Booked Arrival And Departure Is After Booked Departure'() {
     expect:
-    this.booking.conflict(this.arrival.minusDays(4), this.departure.plusDays(4)) == true
+      this.booking.conflict(this.arrival.minusDays(4), this.departure.plusDays(4)) == true
   }
 
   def 'arrival Is Equal Booked Arrival And Departure Is After Booked Departure'() {
     expect:
-    this.booking.conflict(this.arrival, this.departure.plusDays(4)) == true
+      this.booking.conflict(this.arrival, this.departure.plusDays(4)) == true
   }
 
   def 'arrival Is Before Booked Arrival And Departure Is Equal Booked Departure'() {
     expect:
-    this.booking.conflict(this.arrival.minusDays(4), this.departure) == true
+      this.booking.conflict(this.arrival.minusDays(4), this.departure) == true
   }
 
   def 'arrival Is Before Booked Arrival And Departure Is BetweenBooked'() {
     expect:
-    this.booking.conflict(this.arrival.minusDays(4), this.departure.minusDays(3)) == true
+      this.booking.conflict(this.arrival.minusDays(4), this.departure.minusDays(3)) == true
   }
 
   def 'arrival Is Between Booked And Departure Is After Booked Departure'() {
     expect:
-    this.booking.conflict(this.arrival.plusDays(3), this.departure.plusDays(6)) == true
+      this.booking.conflict(this.arrival.plusDays(3), this.departure.plusDays(6)) == true
   }
 
 }
