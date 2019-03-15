d2b88ab546a4ac564243c692c9ecf1cedc172edf
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2019-03-15 13:00:26 +0000

    Fixes #24, BankInterfaceProcessPaymentMethodTest

--- bank/src/test/groovy/pt/ulisboa/tecnico/softeng/bank/services/local/BankInterfaceProcessPaymentMethodSpockTest.groovy
+++ bank/src/test/groovy/pt/ulisboa/tecnico/softeng/bank/services/local/BankInterfaceProcessPaymentMethodSpockTest.groovy
@@ -8,6 +8,7 @@ import pt.ulisboa.tecnico.softeng.bank.domain.SpockRollbackTestAbstractClass
 import pt.ulisboa.tecnico.softeng.bank.exception.BankException
 import pt.ulisboa.tecnico.softeng.bank.services.local.dataobjects.BankOperationData
 import spock.lang.Unroll
+import spock.lang.Shared
 
 class BankInterfaceProcessPaymentMethodSpockTest extends SpockRollbackTestAbstractClass {
   def TRANSACTION_SOURCE = "ADVENTURE"
@@ -15,28 +16,28 @@ class BankInterfaceProcessPaymentMethodSpockTest extends SpockRollbackTestAbstra
 
   def bank
   def account
-  def iban
+  @Shared def iban
 
   @Override
   def populate4Test() {
-    this.bank = new Bank("Money", "BK01")
-    def client = new Client(this.bank, "António")
-    this.account = new Account(this.bank, client)
-    this.iban = this.account.getIBAN()
-    this.account.deposit(500)
+    bank = new Bank("Money", "BK01")
+    def client = new Client(bank, "António")
+    account = new Account(bank, client)
+    iban = account.getIBAN()
+    account.deposit(500)
   }
 
   def 'success'() {
     when:
-    this.account.getIBAN();
-    def newReference = BankInterface.processPayment(new BankOperationData(this.iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
+    account.getIBAN();
+    def newReference = BankInterface.processPayment(new BankOperationData(iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
 
     then:
     newReference != null
     newReference.startsWith("BK01") == true
 
-    this.bank.getOperation(newReference) != null
-    this.bank.getOperation(newReference).getType() == Operation.Type.WITHDRAW
+    bank.getOperation(newReference) != null
+    bank.getOperation(newReference).getType() == Operation.Type.WITHDRAW
   }
 
   def 'success two banks'() {
@@ -49,44 +50,44 @@ class BankInterfaceProcessPaymentMethodSpockTest extends SpockRollbackTestAbstra
 
     when: 
     BankInterface.processPayment(new BankOperationData(otherIban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
-    BankInterface.processPayment(new BankOperationData(this.iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE + "PLUS"))
+    BankInterface.processPayment(new BankOperationData(iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE + "PLUS"))
 
     then:
     otherAccount.getBalance() == 900
-    this.account.getBalance() == 400
+    account.getBalance() == 400
   }
 
   def 'redo an already payed'() {
     when:
-    this.account.getIBAN()
-    def firstReference = BankInterface.processPayment(new BankOperationData(this.iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
-    def secondReference = BankInterface.processPayment(new BankOperationData(this.iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
+    account.getIBAN()
+    def firstReference = BankInterface.processPayment(new BankOperationData(iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
+    def secondReference = BankInterface.processPayment(new BankOperationData(iban, 100, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
     then:
     firstReference == secondReference
-    this.account.getBalance() == 400
+    account.getBalance() == 400
   }
 
-  @Unroll("processPayment: #iban, #val")
+  @Unroll("processPayment: #iba, #val")
   def 'exceptions'() {
     when:
-    BankInterface.processPayment(new BankOperationData(iban, val, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
+    BankInterface.processPayment(new BankOperationData(iba, val, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
       
     then:
     thrown(BankException)
 
     where:
-    iban      | val
+    iba       | val
     null      | 100
     "  "      | 100
-    this.iban | 0
+    iban | 0
     "other"   | 0
   }
 
   def 'one ammount success'() {
     when:
-    BankInterface.processPayment(new BankOperationData(this.iban, 1, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
+    BankInterface.processPayment(new BankOperationData(iban, 1, TRANSACTION_SOURCE, TRANSACTION_REFERENCE))
 
     then: 
-    this.account.getBalance() == 499
+    account.getBalance() == 499
   }
 }
