3b9c3aab6625e2a8f0e8a95b0359f48539ee62fa
Hussein Giva <hussein.giva@gmail.com>
2019-03-22 14:36:36 +0000

    Issue #71 (#92)
    * Migrate ActivityInterfaceReserveActivityMethodTest, closes #59
    
    * Fixes ActivityInterfaceReserveActivityMethodSpockTest, issue #59
    
    * Remove static method calls in Processor.java in Hotel module, closes #75
    
    * Remove static method calls in Processor.java in Hotel module, closes #75
    
    * Fixes Hotel constructor with final keyword, related to issue #75
    
    * Fixed static and final keywords for BookingController.java, related to issue #75
    
    * Fixed static and final keywords for HotelController.java, related to issue #75
    
    * Fixed static and final keywords for RoomController.java, related to issue #75
    
    * Fixed static and final keywords for HotelInterface.java, related to issue #75
    
    * Fixed static and final keywords for BankInterface.java, related to issue #75
    
    * Fixed declaration of new hotelInterface for HotelRestController.java, related to issue #75
    
    * Fixed static and final keywords for TaxInterface.java, related to issue #75
    
    * Fixed declaration of new hotelInterface for HotelInterfaceBulkBookingMethodSpockTest.groovy, related to issue #75
    
    * Fixed declaration of new hotelInterface for HotelInterfaceCancelBookingMethodSpockTest.groovy, related to issue #75
    
    * Fixed declaration of new hotelInterface for HotelInterfaceGetRoomBookingDataMethodSpockTest.groovy, related to issue #75
    
    * Fixed declaration of new hotelInterface for HotelInterfaceReserveRoomMethodSpockTest.groovy, related to issue #75
    
    * Fixed Hotel constructor due to processor, related to issue #75
    
    * Migrate ProcessSubmitBookingMethodTest, closes #71
    
    * Deleted java tests, issue #75
    
    * Migrate HotelInterfaceBulkBookingMethodTest, closes #72
    
    * Migrate HotelInterfaceCancelBookingMethodTest, closes #73
    
    * Fixes ProcessorSubmitBookingMethodSpockTest.groovy, issue #71
    
    * Fixes Hotel.java, issue #75
    
    * Fixes Booking.java, related to issue #75
    
    * Fixes Hotel.java, related to issue #75
    
    * Fixes Processor.java, related to issue #75
    
    * Fixes HotelInterface.java, related to issue #75
    
    * Fixes HotelInterfaceBulkBookingMethodSpockTest2.groovy, issue #73
    
    * Fixes HotelInterfaceBulkBookingMethodSpockTest2.groovy, issue #72
    
    * Fixes HotelInterfaceCancelBookingMethodSpockTest2.groovy, issue #73
    
    * Fixes HotelInterface.java, related to issue #71
    
    * Changes names for tests, related to issue #71
    
    * Fixes HotelInterface.java, related to issue #71
    
    * Fixes HotelInterface.java, related to issue #71

---
+++ hotel/src/test/groovy/pt/ulisboa/tecnico/softeng/hotel/services/local/HotelInterfaceCancelBookingMethodAlternativeSpockTest.groovy
@@ -0,0 +1,92 @@
+package pt.ulisboa.tecnico.softeng.hotel.services.local
+
+import org.joda.time.LocalDate
+
+import pt.ulisboa.tecnico.softeng.hotel.domain.Hotel
+import pt.ulisboa.tecnico.softeng.hotel.domain.Processor
+import pt.ulisboa.tecnico.softeng.hotel.domain.SpockRollbackTestAbstractClass
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException
+import pt.ulisboa.tecnico.softeng.hotel.services.remote.BankInterface
+import pt.ulisboa.tecnico.softeng.hotel.services.remote.TaxInterface
+
+
+class HotelInterfaceCancelBookingMethodAlternativeSpockTest extends SpockRollbackTestAbstractClass {
+	def arrival = new LocalDate(2016, 12, 19)
+	def departure = new LocalDate(2016, 12, 21)
+	def NIF_BUYER = "123456789"
+	def IBAN_BUYER = "IBAN_BUYER"
+
+	def hotel
+	def room
+	def booking
+	def taxInterface
+	def bankInterface
+	def hotelInterface
+
+	@Override
+	def populate4Test() {
+		bankInterface = Mock(BankInterface)
+		taxInterface = Mock(TaxInterface)
+		hotelInterface = new HotelInterface()
+		def processor = new Processor(bankInterface, taxInterface)
+
+		hotel = new Hotel("XPTO123", "Paris", "NIF", "IBAN", 20.0, 30.0, processor)
+		room = new Room(hotel, "01", Type.DOUBLE)
+		booking = room.reserve(Type.DOUBLE, arrival, departure, NIF_BUYER, IBAN_BUYER)
+	}
+
+	def "success"() {
+		when:
+		def cancel = hotelInterface.cancelBooking(booking.getReference())
+
+		then:
+		booking.isCancelled()
+		booking.getCancellation() == cancel
+	}
+
+	def "doesNotExist"() {
+		when:
+		hotelInterface.cancelBooking("XPTO")
+
+		then:
+		thrown(HotelException)
+	}
+
+	def "nullReference"() {
+		when:
+		hotelInterface.cancelBooking(null)
+
+		then:
+		thrown(HotelException)
+	}
+
+	def "emptyReference"() {
+		when:
+		hotelInterface.cancelBooking("")
+
+		then:
+		thrown(HotelException)
+	}
+
+	def "successIntegration"() {
+		when:
+		def cancel = hotelInterface.cancelBooking(booking.getReference())
+
+		then:
+		1 * taxInterface.cancelInvoice(_)
+		and:
+		booking.isCancelled()
+		booking.getCancellation() == cancel
+	}
+
+	def "doesNotExistIntegration"() {
+		given:
+		0 * taxInterface.cancelInvoice(_)
+		when:
+		hotelInterface.cancelBooking("XPTO")
+		then:
+		thrown(HotelException)
+	}
+}
